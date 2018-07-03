684e17b855580cddfa0cac75f5737027dceb2d15
Daniel Aragão <danielfilhoce@gmail.com>
2018-07-02 21:06:03 -0300

    aula 15

--- database/Connection.php
+++ database/Connection.php
@@ -1,9 +1,16 @@
 <?php 
 
 class Connection{
-    public static function make(){
+    public static function make($config){
         try{
             $pdo = new PDO('mysql:127.0.0.1;dbname=mytodo;', 'root', '');
+
+            return new PDO(
+                $config['connnection'] . ';dbname' . $config['name'],
+                $config['username'],
+                $config['password'],
+                $config['options']
+            );
             
         }catch(PDOException $e){
             die('Could not connect to database: ' . $e->getMessage());
